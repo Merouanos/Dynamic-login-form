33ce96c7922a997e33642d98d44395be3dbf16e9
Merouanos <jessemerouane@gmail.com>
2025-03-21 15:04:50 +0100

    init all

---
+++ CheckDb.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$host = "localhost";
+$username = "Merouanos";
+$password = "Mergito1000#";
+$database = "MerouaneSchool";
+
+
+$sql = mysqli_connect($host, $username, $password, $database);
+
+
+if($sql){
+    $sql->query("C")
+
+
+
+
+}
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Merouane School</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="" type="icon">
+
+</head>
+<body>
+    <header>
+        <img src="#" alt="Merouanos logo">
+
+    </header>
+    <main>
+        <h1>Welcome to Merouane School</h1>
+        <form action="CheckDb.php" method="get">
+            <fieldset>
+                    <legend></legend>
+                    <label for="name">Username:*</label>
+                    <input type="text" name="username" placeholder="James Bond">
+                    <label for="name">Password:*</label>
+                    <input type="password" name="password" >
+
+
+            </fieldset>
+        </form>
+
+    </main>
+
+
+</body>
+</html>
